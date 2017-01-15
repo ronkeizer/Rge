320afdf5155a4883735279c8c36218ea16061d38
ronkeizer <ronkeizer@gmail.com>
2017-01-14 19:52:57 -0800

    misc fixes

--- R/ge_qstat.R
+++ R/ge_qstat.R
@@ -2,34 +2,52 @@
 #'
 #' @param cmd qstat command, default `qstat`
 #' @param flags add flags
-#' @param user show only for specific user
-#' @param filter filter list of column names and filter values
+#' @param user specify user
+#' @param queue specify queue
+#' @param filter filter (afterwards) list of column names and filter values
 #' @param exact filter using exact match or partial match? `TRUE` by default
 #' @export
-sge_qstat <- function(
+ge_qstat <- function(
   cmd = "qstat",
   flags = "",
   user = NULL,
+  queue = NULL,
   filter = NULL,
-  exact = TRUE) {
+  exact = TRUE,
+  ext = FALSE,
+  raw = FALSE) {
   flags <- paste(flags, " -xml")
   if(!is.null(user)) {
     flags <- paste(flags, "-u", user)
   }
+  if(!is.null(queue)) {
+    flags <- paste(flags, "-q", queue)
+  }
+  if(ext) {
+    flags <- paste(flags, "-ext")
+  }
   out <- paste(system(paste(cmd, flags), intern=TRUE), collapse = "")
+  if(raw) {
+    return(out)
+  }
   data_all <- xml2::as_list(xml2::read_xml(out), )
   all <- c()
   for(i in seq(data_all$job_info)) {
     tmp <- data_all$job_info[i]$job_list
-    if(length(tmp$queue_name) == 0) { tmp$queue_name <- "" }
+    for(j in tmp) {
+      if(length(tmp) == 0) {
+        tmp[[j]] <- ""
+      }
+    }
     all <- rbind(all, unlist(tmp))
   }
   all <- data.frame(all)
   all$JAT_prio <- as.numeric(all$JAT_prio)
   all$slots <- as.numeric(all$slots)
-  all$JB_submission_time <- as.POSIXct(all$JB_submission_time)
-  cols <- c("job-ID", "prior", "name", "user", "state", "t_submit_start", "queue", "slots", "ja-task-ID")
-  colnames(all) <- cols[1:length(all[1,])]
+  if(!ext) {
+    cols <- c("job-ID", "prior", "name", "user", "state", "t_submit_start", "queue", "slots", "ja-task-ID")
+    colnames(all) <- cols[1:length(all[1,])]
+  }
   if(!is.null(filter)) {
     for(i in 1:length(names(filter))) {
       if(exact) {
--- R/ge_qsub.R
+++ R/ge_qsub.R
@@ -1,11 +1,20 @@
-#' Submit job to SGE
+#' Submit job to GridEngine
 #'
 #' @param cmd command to execute
 #' @param script_file script file called by submit command
 #' @param submit_cmd command used to submit SGE job (default `qsub`)
+#' @param queue queue name
+#' @param jobId job name. If blank assigned automatically by grid engine
 #'
 #' @export
-sge_qsub <- function(cmd, script_file = "job.sh", submit_cmd = "qsub") {
+ge_qsub <- function(
+  cmd,
+  script_file = "job.sh",
+  submit_cmd = "qsub",
+  queue = NULL,
+  name = NULL,
+  prefix = NULL
+  ) {
     script <- c("#!/bin/bash", "#", "#$ -cwd", "#$ -j y", "#$ -S /bin/bash","#",
                 cmd)
     fileConn <- file(script_file)
@@ -13,6 +22,15 @@ sge_qsub <- function(cmd, script_file = "job.sh", submit_cmd = "qsub") {
     close(fileConn)
     out <- system(paste(submit_cmd = submit_cmd, script_file), intern=TRUE)
     jobId <- NULL
+    if(is.null(name)) {
+      random_string <- function(len = 12) { return(paste(sample(c(rep(0:9,each=5),LETTERS,letters),len,replace=TRUE),collapse='')) }
+      name <- random_string(8)
+      job <- stringr::str_split(cmd, " ")[[1]][1]
+      name <- paste0(job, "_", random_string(8))
+    }
+    if(!is.null(prefix)) {
+      name <- paste0(prefix, "_", job)
+    }
     if(stringr::str_detect(out[1], "Your job")) {
       jobId <- stringr::str_split(
         stringr::str_split(out[1], "Your job ")[[1]][2],
